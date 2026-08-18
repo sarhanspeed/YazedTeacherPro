eec5aea6e1105326c7d1979ac11bae92bd3e528c
sarhanspeed <99893528+sarhanspeed@users.noreply.github.com>
2026-08-18 22:12:53 +0300

    Bump YazedTeacherPro to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sarhansoftware.yazedteacherpro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
